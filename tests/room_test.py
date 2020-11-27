4a35abefb845b99fafabec245993ee33df6dfeb0
emCog <robin.leonard@gmail.com>
2020-11-27 22:26:33 +0000

    all basic tests written

--- tests/room_test.py
+++ tests/room_test.py
@@ -6,6 +6,9 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.wembley = Room('Wembley', 9)
         self.glasto = Room('Glastonbury', 21)
+        self.open_mic_01 = Room('Open mic Monday', 4)
+        self.open_mic_02 = Room('Open mic Tuesday', 5)
+        self.open_mic_03 = Room('Open mic Wednesday', 6)
 
     def test_room_has_name(self):
         self.assertEqual('Wembley', self.wembley.name)
@@ -13,8 +16,15 @@ class TestRoom(unittest.TestCase):
     def test_room_has_capacity(self):
         self.assertEqual(21, self.glasto.capacity)
 
-    # def test_check_in(self):
-        
+    def test_check_in(self):
+      self.assertEqual(4, self.open_mic_01.check_in)
+
+    def test_check_out(self):
+        self.assertEqual(4, self.open_mic_01.check_out)
+
+    def test_add_song(self):
+        self.assertEqual('She Drives Me Crazy' self.open_mic_01.add_song)
+
 
     # def test_check_out(self):
 
